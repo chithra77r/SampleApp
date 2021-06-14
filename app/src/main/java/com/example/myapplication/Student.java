8bf75ffda4308d54e61781878125de9f5a3a34f4
chithra <chithracool13@gmail.com>
2021-06-14 23:27:17 +0530

    entity and roomdependency added

--- app/src/main/java/com/example/myapplication/Student.java
+++ app/src/main/java/com/example/myapplication/Student.java
@@ -1,5 +1,18 @@
 package com.example.myapplication;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity
 public class Student {
+    @PrimaryKey
+    int id;
+    @ColumnInfo(name = "s_name")
     String name;
+    @ColumnInfo(name = "s_age")
+    int age;
+    @Ignore
+    int dummy;
 }
